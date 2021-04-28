5a90b6f4089f6ecddefedd7b4ae7a3fbf28a98d0
JeonghyeonJeon <36718497+JeonghyeonJeon@users.noreply.github.com>
2021-04-28 16:55:24 +0900

    Add files via upload

---
+++ src/pages/ThirdPage.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import checkImage from '../images/checkImage.PNG';
+import TransferImage from '../images/TransferImage.PNG';
+import '../css/ThirdPage.css'
+import {Link} from 'react-router-dom'
+
+function ThirdPage() {
+    return (
+        <div>
+          <img src={TransferImage}alt="Trans" class="Trans"/>
+            <div className="screen">
+                <img src = {checkImage}
+                  width = '100px'
+                  height='100px' />
+                <div>
+                <input className="userName" type="userName"/>
+                <div className="userNameText">님에게</div>
+                </div>
+                <div>
+                    <div className="amount"></div>
+                    <div className="transferMessage">이체가 성공적으로 완료되었습니다</div>
+                </div>
+                <div>
+                    <label className ="withdrawerAccontText"> 받는 계좌</label>
+                    <input className="withdrawerInput" type="withdrawerAccount" />
+                </div>
+                <div>
+                <div className="recentTransferRecordName">최근 거래내역</div>
+                <div className="recentTransferRecord"></div>
+                </div>
+                <button className="TransferRecordBtn">거래내역조회</button>
+                <button className="checkBtn">확인</button>
+            </div>
+        </div>
+    );
+}
+
+export default ThirdPage
