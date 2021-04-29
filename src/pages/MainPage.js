607233b64e32261e4bb11bc4dd9ba90c704fca80
ayoung0073 <ayong0310@naver.com>
2021-04-29 19:44:00 +0900

    Fix : 메인 페이지 CSS 수정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,28 +1,37 @@
 import React from 'react'
 import '../css/MainPage.css'
 import {Link} from 'react-router-dom'
+import MainImage from '../images/MainPicture.png';
+
 
 function MainPage(props) {
     return (
       <div>
-        <div className="bg">
+        <style>
+
+        </style>
+        <div className="nav">
             <span className="title">Holy Connect</span> 
         </div>
-        <div className="mainBody">
-             <div className="mainContent">
-                Be with you, <br/>
-                Be valuable.
-             </div>
-             <div className="subContent">
-                Holy Connect 가치를 부여하다
-             </div>
-            <Link to="/second">
-                <button className="btn-first">현금</button>
-            </Link>
-            <button className="btn-second">
-                스케줄
-            </button>
+        {/* <img src={MainImage} height="1000px" alt="MainImage" class="MainImage"> */}
+        <div className="bg">
+            <div className="mainBody">
+                <div className="mainContent">
+                    Be with you, <br/>
+                    Be valuable.
+                </div>
+                <div className="subContent">
+                    Holy Connect, 가치를 부여하다
+                </div>
+                <Link to="/second">
+                    <button className="btn-first">현금</button>
+                </Link>
+                <button className="btn-second">
+                    스케줄
+                </button>
+            </div>
         </div>
+        {/* </img> */}
       </div>
     );
 }
