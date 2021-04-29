b3fc6898e5992862765a7e92fabe7e2ad730525a
ayoung0073 <ayong0310@naver.com>
2021-04-29 15:26:51 +0900

    Add : 사용자 정보 GET

--- src/pages/SecondPage.js
+++ src/pages/SecondPage.js
@@ -1,16 +1,23 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import '../css/SecondPage.css'
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
 function SecondPage() {
+    const [User, setUser] = useState([]);
+
+    const [Account, setAccount] = useState('');
     useEffect(() => {
-        axios.get('http://127.0.0.1:5000/api/info', {
+        axios.get('http://192.168.0.21:5000/api/info', {
             headers: {"Authorization" : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXJJZCI6Mn0sImlhdCI6MTYxOTY1ODMxMSwiZXhwIjoxNjE5Njg3MTExfQ.eouMk7rkaiW57sYE-ZupbBzIkbdImLdXB09VsI2UzNY'}
-        }) /* 사이트URL/Third 로 받아오면 됨*/
+        }) 
             .then(response => {
-                console.log(response)
-            });
+                if (response.data.success){
+                    console.log(response.data.data)
+                    setUser(response.data.data);
+                }
+            })
     }, []);  
     return(
         <div className="BG">
@@ -19,30 +26,39 @@ function SecondPage() {
                 가치(Value)를 공유하는 삶 
                 </div>
                 <div className="Holy-Connect-is-always-being-with-you">
-                Holy Connect is always being with you
+                    Holy Connect is always being with you
                 </div>
             </div>
 
             <div className="RightSpace">
+
                 <div>
-                    <div>
-                    <button className="nameBt1">기명</button>
-                    <button className="nameBt2">무기명</button>
-                    </div>
-                    <label className="SendMoney">보낼금액</label>
-                    <input type="text" className="Rectangle-2"/>
+                    <label className="named">기명</label>
+                    <input type="checkbox" className="nameBt1"/>
+                    <label className="named">무기명</label>
+                    <input type="checkbox" className="nameBt2"/>
                 </div>
+                <label className="SendMoney">보낼금액</label>
+                <input type="text" className="Rectangle-2"/>
                 <div>
                 <label className="Account">
                     출금계좌
                 </label>
                 <select>
-                    <option value="kb bank">kb bank</option>
-                    <option value="kakao bank">kakao bank</option>
+                    {User.length != 0 &&                
+                        User.res_list.map((element) => {
+                           return <option value={element.fintech_use_num} selected>{element.account_alias}</option>
+                        })
+                    }
                 </select>
-                <input type="text" value="123 2434 235435" className="AccountNumber"></input>
+                {User.length != 0 &&   
+                    <input type="text" value={User.res_list[0].account_num_masked} className="AccountNumber"></input>
+                }   
+
                 </div>
-            <input className="submit" type="submit"></input>
+                <Link to="/third">
+                    <input className="submit" type="submit"></input>
+                </Link>
             </div>
         </div>
     
